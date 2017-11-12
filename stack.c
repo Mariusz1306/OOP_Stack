6ec898a98f91f86c554c54fb0faed371da75efc0
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2017-11-12 01:11:29 +0100

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,40 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "stack.h"
+
+extern int stacksize;
+
+void init(struct stack_handle* s)
+{
+	s->top=0;
+	s->dane  = calloc(stacksize, sizeof(int));
+}
+
+void finalize(struct stack_handle* s)
+{
+    free(s->dane);
+    free(s);
+}
+
+void clear(struct stack_handle* s)
+{
+	s->top=0;
+}
+
+void push(struct stack_handle* s,int a)
+{
+        //assert(s->top<stacksize);
+        if (s->top<stacksize){
+            stacksize = stacksize*2;
+            s->dane = realloc(s->dane, stacksize * sizeof(int));
+        }
+	s->dane[s->top++]=a;
+}
+
+int pop(struct stack_handle* s)
+{
+	assert(s->top>0);
+	return s->dane[--s->top];
+}
+
---
+++ teststack.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "stack.h"
+
+#define initSTACKSIZE 5
+int stacksize = initSTACKSIZE;
+
+int main()
+{
+
+	struct stack_handle s1;
+	init(&s1);
+	push(&s1,1);
+	push(&s1,2);
+	push(&s1,3);
+	push(&s1,4);
+	push(&s1,5);
+	push(&s1,6);
+	push(&s1,7);
+	push(&s1,8);
+	push(&s1,9);
+	push(&s1,10);
+	push(&s1,11);
+	push(&s1,12);
+	push(&s1,13);
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	printf("%d\n",pop(&s1));
+	finalize(&s1);
+	return 0;
+}
