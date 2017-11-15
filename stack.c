c22aba6da2986b3225c2b447416b931e410ea9a6
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2017-11-15 08:01:08 +0100

    Add files via upload

--- stack.c
+++ stack.c
@@ -3,37 +3,46 @@
 #include <stdio.h>
 #include "stack.h"
 
-extern int stacksize;
+#define initSTACKSIZE 5
+int init_stacksize = initSTACKSIZE;
+int number_of_stacks = 0;
+int *ptr_stacksizes;
 
-void init(struct stack_handle* s)
-{
+void init(struct stack_handle* s){
+    number_of_stacks++;
+    s->stack_number = number_of_stacks;
+    if (number_of_stacks == 1)
+        ptr_stacksizes = malloc(number_of_stacks * sizeof(int));
+    else
+        ptr_stacksizes = realloc(ptr_stacksizes, number_of_stacks * sizeof(int));
+    *(ptr_stacksizes+s->stack_number-1) = init_stacksize;
 	s->top=0;
-	s->dane  = calloc(stacksize, sizeof(int));
+	s->dane  = calloc(init_stacksize, sizeof(int));
 }
 
-void finalize(struct stack_handle* s)
-{
+void finalize(struct stack_handle* s){
+    --number_of_stacks;
+	ptr_stacksizes = realloc(ptr_stacksizes, number_of_stacks * sizeof(int));
     free(s->dane);
     free(s);
 }
 
-void clear(struct stack_handle* s)
-{
+void clear(struct stack_handle* s){
 	s->top=0;
 }
 
-void push(struct stack_handle* s,int a)
-{
+void push(struct stack_handle* s,int a){
         //assert(s->top<stacksize);
-        if (s->top<stacksize){
-            stacksize = stacksize*2;
-            s->dane = realloc(s->dane, stacksize * sizeof(int));
+    int temp_stacksize = *(ptr_stacksizes+s->stack_number-1);
+        if (s->top>=temp_stacksize){
+            temp_stacksize = temp_stacksize * 2;
+            s->dane = realloc(s->dane, temp_stacksize * sizeof(int));
+            *(ptr_stacksizes+s->stack_number-1) = temp_stacksize;
         }
 	s->dane[s->top++]=a;
 }
 
-int pop(struct stack_handle* s)
-{
+int pop(struct stack_handle* s){
 	assert(s->top>0);
 	return s->dane[--s->top];
 }
--- teststack.c
+++ teststack.c
@@ -1,40 +1,21 @@
 #include <stdio.h>
 #include "stack.h"
 
-#define initSTACKSIZE 5
-int stacksize = initSTACKSIZE;
-
-int main()
-{
-
+int main(){
+    int i;
 	struct stack_handle s1;
+	struct stack_handle s2;
 	init(&s1);
-	push(&s1,1);
-	push(&s1,2);
-	push(&s1,3);
-	push(&s1,4);
-	push(&s1,5);
-	push(&s1,6);
-	push(&s1,7);
-	push(&s1,8);
-	push(&s1,9);
-	push(&s1,10);
-	push(&s1,11);
-	push(&s1,12);
-	push(&s1,13);
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
-	printf("%d\n",pop(&s1));
+	init(&s2);
+	for (i = 1; i <= 15; i++)
+        push(&s1,i);
+    for (i = 1; i <= 15; i++)
+        printf("%d\n",pop(&s1));
+    for (i = 1; i <= 30; i++)
+        push(&s2,i);
+    for (i = 1; i <= 30; i++)
+        printf("%d\n",pop(&s2));
 	finalize(&s1);
+	finalize(&s2);
 	return 0;
 }
